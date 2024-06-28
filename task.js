e4064c9d8da10160a60107146efb550c83904df9
jebasingh108 <jebasingh108@gmail.com>
2024-06-28 18:49:37 +0530

    mediora

---
+++ task.js
@@ -0,0 +1,63 @@
+document.getElementById('heartIcon').addEventListener('click', function() {
+    this.classList.toggle('red-background');
+});
+
+document.getElementById('anotherIcon').addEventListener('click', function() {
+    this.classList.toggle('red-background');
+});
+
+document.getElementById('heartIcon1').addEventListener('click', function() {
+    this.classList.toggle('red-background');
+});
+
+document.getElementById('anotherIcon1').addEventListener('click', function() {
+    this.classList.toggle('red-background');
+});
+
+document.getElementById('heartIcon2').addEventListener('click', function() {
+    this.classList.toggle('red-background');
+});
+
+document.getElementById('anotherIcon2').addEventListener('click', function() {
+    this.classList.toggle('red-background');
+});
+
+document.getElementById('heartIcon3').addEventListener('click', function() {
+    this.classList.toggle('red-background');
+});
+
+document.getElementById('anotherIcon3').addEventListener('click', function() {
+    this.classList.toggle('red-background');
+});
+
+document.getElementById('heartIcon4').addEventListener('click', function() {
+    this.classList.toggle('red-background');
+});
+
+document.getElementById('anotherIcon4').addEventListener('click', function() {
+    this.classList.toggle('red-background');
+});
+
+document.getElementById('heartIcon5').addEventListener('click', function() {
+    this.classList.toggle('red-background');
+});
+
+document.getElementById('anotherIcon5').addEventListener('click', function() {
+    this.classList.toggle('red-background');
+});
+
+document.getElementById('heartIcon6').addEventListener('click', function() {
+    this.classList.toggle('red-background');
+});
+
+document.getElementById('anotherIcon6').addEventListener('click', function() {
+    this.classList.toggle('red-background');
+});
+
+document.getElementById('heartIcon7').addEventListener('click', function() {
+    this.classList.toggle('red-background');
+});
+
+document.getElementById('anotherIcon7').addEventListener('click', function() {
+    this.classList.toggle('red-background');
+});
